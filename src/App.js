c67b9a23f04ea2123487733d12089ff79cd4c2d2
Brady <dukartbrady@gmail.com>
2019-11-27 10:32:32 -0600

    added logo to assets folder

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <h1 className="header">Hello World</h1>
     </div>
   );
 }
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import Logo from "../../assets/logo.svg";
 
 const Header = () => {
     return (
-        <div className="container header">
+        <div className="header">
+            <div className="container">
+            </div>
             <h1>This is the header</h1>
             <p>This is a p tag</p>
         </div>
