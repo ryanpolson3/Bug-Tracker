fea6f090de2e4b92975d9c07f7636199ec18d196
Ryan Olson <ryanpolson3@gmail.com>
2020-04-11 17:32:14 -0500

    Updated header bar.

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import Logo from "../../assets/logo.svg";
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div className="header">
             <div className="row no-gutters">
-                <div className="col-6">
-                    <img src={Logo} className="header--logo" alt="bug-tracker-logo"/>
+                <div className="logo col-2">
+                    <img src={Logo} className="" alt="App Logo"/>
                 </div>
-                <div className="col-6">
-                    <ul className="header--item-list__container float-right">
-                        <li className="header--item-list__item">Live</li>
-                        <li className="header--item-list__item">Daily</li>
-                        <li className="header--item-list__item">Weekly</li>
-                        <li className="header--item-list__item">Monthly</li>
-                        <li className="header--item-list__item">Yearly</li>
+                <div className="nav col-8 justify-content-end">
+                    <ul className="">
+                        <li className="">Live</li>
+                        <li className="">Daily</li>
+                        <li className="">Weekly</li>
+                        <li className="">Monthly</li>
+                        <li className="">Yearly</li>
                     </ul>
+                </div>
+                <div className="col-2 login">
+                    {/*<img src={personIcon} className="" alt="Person Icon" />
+                    <p>User Name</p>*/}
+                    <p><span className="fa fa-user fa-2x"></span> User Name</p>
                 </div>
             </div>
         </div>
